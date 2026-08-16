0d6d512d647545bc1144f65c6db989e0d56359c2
Ubuntu <ubuntu@ip-172-31-35-101.ap-southeast-2.compute.internal>
2026-08-16 11:41:57 +0000

    ci: add deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,22 @@
 
 set -e
 
-cd /home/ubuntu/ecommerce
-
 echo "===== DEPLOY START ====="
 
+cd /home/ubuntu/ecommerce
+
+echo "[1/5] Pull latest code..."
 git pull origin main
 
-docker compose up -d --build
+echo "[2/5] Building containers..."
+docker compose build
 
+echo "[3/5] Restarting application..."
+docker compose up -d
+
+echo "[4/5] Cleaning old Docker images..."
 docker image prune -f
+
+echo "[5/5] Deploy completed."
 
 echo "===== DEPLOY SUCCESS ====="
